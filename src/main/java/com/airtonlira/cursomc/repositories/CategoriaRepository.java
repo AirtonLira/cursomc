3364823df310bb6f3e2dd7aabda54f1ec025b13a
Airton Lira <airtonlirajr@gmail.com>
2019-11-02 15:55:15 -0300

    inicialização automatica dos dados de categoria

---
+++ src/main/java/com/airtonlira/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,12 @@
+package com.airtonlira.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.airtonlira.cursomc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+
+		
+}
